703125c1aff08b204c5ff2b8f9ed8bdbc53decb5
Rocky <44615614+softwarecrash@users.noreply.github.com>
2023-08-08 10:59:54 +0200

    Update VeDirectDataList.h

--- src/VeDirectDataList.h
+++ src/VeDirectDataList.h
@@ -1,9 +1,20 @@
 const char* VePrettyData[][3]{
 // get name, pretify name, value operator
-{"PID","Device_model",""},
-{"V","Voltage", "100",},
-
-{"SOC", "SOC", "100"},
-
+{"V","Voltage", "100",}, // most device deliver this, display in webUI
+{"V2","Voltage_2", "100",},
+{"V3","Voltage_3", "100",},
 {"VS","Starter_voltage", "100",}
+
+{"I","Battery_current","100"}, // most device deliver this, display in webUI
+{"I2","Battery_current_2","100"},
+{"I3","Battery_current_3","100"},
+  
+{"SOC", "SOC", "100"},
+{"TTG","Time_to_go",""},
+{"ALARM","Alarm",""},
+{"RELAY","Relay",""}, // most device deliver this, display in webUI
+{"AR", "Alarm_code", ""},
+{"OR", "Off_reason", ""},
+
+{"PID","Device_model",""},
 };
