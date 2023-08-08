884b46c064892a08c562dd8e954c36b3d22b07dc
Rocky <44615614+softwarecrash@users.noreply.github.com>
2023-08-08 11:14:09 +0200

    Update VeDirectDataList.h

--- src/VeDirectDataList.h
+++ src/VeDirectDataList.h
@@ -1,6 +1,6 @@
 const char* VePrettyData[][3]{
 // get name, pretify name, value operator
-{"V","Voltage", "100",}, // most device deliver this, display in webUI
+{"V","Voltage", "100",}, // display in webUI | Dont edit
 {"V2","Voltage_2", "100",},
 {"V3","Voltage_3", "100",},
 {"VS","Starter_voltage", "100",},
@@ -8,17 +8,27 @@ const char* VePrettyData[][3]{
 {"DM","Mid_deviation", "",},
 {"VPV","Panel_voltage", "100",},
 {"PPV","Panel_power", "",},
-  
-{"I","Battery_current","100"}, // most device deliver this, display in webUI
+{"I","Battery_current","100"}, // display in webUI | Dont edit
 {"I2","Battery_current_2","100"},
 {"I3","Battery_current_3","100"},
-  
+{"IL","Load_current","100"},
+{"LOAD","Load_output_state",""},
+{"T","Load_output_state",""},
+{"P","Instantaneous_power",""},
+{"CE","Consumed_Amp_Hours","100"},
 {"SOC", "SOC", "100"},
 {"TTG","Time_to_go",""},
 {"ALARM","Alarm",""},
-{"RELAY","Relay",""}, // most device deliver this, display in webUI
+{"RELAY","Relay",""}, // display in webUI | Dont edit
 {"AR", "Alarm_code", ""},
 {"OR", "Off_reason", ""},
+{"H1", "Deepest_discharge", "100"},
+{"H2", "Last_discharge", "100"},
+{"H3", "Average_discharge", "100"},
+{"H4", "Charge_cycles", ""},
+{"H5", "Full_discharges", ""},
+{"H6", "Cumulative_Ah_drawn", "100"},
+  
 
 {"PID","Device_model",""},
 };
