026af1b6eb937f626d2a3190f03f8f4d461e9fc2
ManuA <webmaster@all-solutions.de>
2023-10-13 12:53:55 +0200

    DeviceList extended

--- src/VeDirectDeviceList.h
+++ src/VeDirectDeviceList.h
@@ -49,113 +49,111 @@ static const char * const VeDirectDeviceList[][2]
 {"0XA069","SmartSolar MPPT 250|70 rev2"},
 {"0XA06A","SmartSolar MPPT 150|45 rev2"},
 {"0XA06B","SmartSolar MPPT 150|60 rev2"},
+{"0XA06C","SmartSolar MPPT 150|70 rev2"},
+{"0XA06D","SmartSolar MPPT 150|85 rev3"},
+{"0XA06E","SmartSolar MPPT 150|100 rev3"},
+{"0XA06F","BlueSolar MPPT 150|45 rev2"},
+{"0XA070","BlueSolar MPPT 150|60 rev2"},
+{"0XA071","BlueSolar MPPT 150|70 rev2"},
+{"0XA072","BlueSolar MPPT 150/45 rev3"},
+{"0XA073","SmartSolar MPPT 150/45 rev3"},
+{"0XA074","SmartSolar MPPT 75/10 rev2"},
+{"0XA075","SmartSolar MPPT 75/15 rev2"},
+{"0XA076","BlueSolar MPPT 100/30 rev3"},
+{"0XA077","BlueSolar MPPT 100/50 rev3"},
+{"0XA078","BlueSolar MPPT 150/35 rev3"},
+{"0XA079","BlueSolar MPPT 75/10 rev2"},
+{"0XA07A","BlueSolar MPPT 75/15 rev2"},
+{"0XA07B","BlueSolar MPPT 100/15 rev2"},
+{"0XA07C","BlueSolar MPPT 75/10 rev3"},
+{"0XA07D","BlueSolar MPPT 75/15 rev3"},
+{"0XA07E","SmartSolar MPPT 100/30 12V"},
+{"0XA07F","All-In-1 SmartSolar MPPT 75/15 12V"},
+{"0XA102","SmartSolar MPPT VE.Can 150/70"},
+{"0XA103","SmartSolar MPPT VE.Can 150/45"},
+{"0XA104","SmartSolar MPPT VE.Can 150/60"},
+{"0XA105","SmartSolar MPPT VE.Can 150/85"},
+{"0XA106","SmartSolar MPPT VE.Can 150/100"},
+{"0XA107","SmartSolar MPPT VE.Can 250/45"},
+{"0XA108","SmartSolar MPPT VE.Can 250/60"},
+{"0XA109","SmartSolar MPPT VE.Can 250/70"},
+{"0XA10A","SmartSolar MPPT VE.Can 250/85"},
+{"0XA10B","SmartSolar MPPT VE.Can 250/100"},
+{"0XA10C","SmartSolar MPPT VE.Can 150/70 rev2"},
+{"0XA10D","SmartSolar MPPT VE.Can 150/85 rev2"},
+{"0XA10E","SmartSolar MPPT VE.Can 150/100 rev2"},
+{"0XA10F","BlueSolar MPPT VE.Can 150/100"},
+{"0XA112","BlueSolar MPPT VE.Can 250/70"},
+{"0XA113","BlueSolar MPPT VE.Can 250/100"},
+{"0XA114","SmartSolar MPPT VE.Can 250/70 rev2"},
+{"0XA115","SmartSolar MPPT VE.Can 250/100 rev2"},
+{"0XA116","SmartSolar MPPT VE.Can 250/85 rev2"},
+{"0XA117","BlueSolar MPPT VE.Can 150/100 rev2"},
+{"0XA201","Phoenix Inverter 12V 250VA 230V"},
+{"0XA202","Phoenix Inverter 24V 250VA 230V"},
+{"0XA204","Phoenix Inverter 48V 250VA 230V"},
+{"0XA211","Phoenix Inverter 12V 375VA 230V"},
+{"0XA212","Phoenix Inverter 24V 375VA 230V"},
+{"0XA214","Phoenix Inverter 48V 375VA 230V"},
+{"0XA221","Phoenix Inverter 12V 500VA 230V"},
+{"0XA222","Phoenix Inverter 24V 500VA 230V"},
+{"0XA224","Phoenix Inverter 48V 500VA 230V"},
+{"0XA231","Phoenix Inverter 12V 250VA 230V"},
+{"0XA232","Phoenix Inverter 24V 250VA 230V"},
+{"0XA234","Phoenix Inverter 48V 250VA 230V"},
+{"0XA239","Phoenix Inverter 12V 250VA 120V"},
+{"0XA23A","Phoenix Inverter 24V 250VA 120V"},
+{"0XA23C","Phoenix Inverter 48V 250VA 120V"},
+{"0XA241","Phoenix Inverter 12V 375VA 230V"},
+{"0XA242","Phoenix Inverter 24V 375VA 230V"},
+{"0XA244","Phoenix Inverter 48V 375VA 230V"},
+{"0XA249","Phoenix Inverter 12V 375VA 120V"},
+{"0XA24A","Phoenix Inverter 24V 375VA 120V"},
+{"0XA24C","Phoenix Inverter 48V 375VA 120V"},
+{"0XA251","Phoenix Inverter 12V 500VA 230V"},
+{"0XA252","Phoenix Inverter 24V 500VA 230V"},
+{"0XA254","Phoenix Inverter 48V 500VA 230V"},
+{"0XA259","Phoenix Inverter 12V 500VA 120V"},
+{"0XA25A","Phoenix Inverter 24V 500VA 120V"},
+{"0XA25C","Phoenix Inverter 48V 500VA 120V"},
+{"0XA261","Phoenix Inverter 12V 800VA 230V"},
+{"0XA262","Phoenix Inverter 24V 800VA 230V"},
+{"0XA264","Phoenix Inverter 48V 800VA 230V"},
+{"0XA269","Phoenix Inverter 12V 800VA 120V"},
+{"0XA26A","Phoenix Inverter 24V 800VA 120V"},
+{"0XA26C","Phoenix Inverter 48V 800VA 120V"},
+{"0XA271","Phoenix Inverter 12V 1200VA 230V"},
+{"0XA272","Phoenix Inverter 24V 1200VA 230V"},
+{"0XA274","Phoenix Inverter 48V 1200VA 230V"},
+{"0XA279","Phoenix Inverter 12V 1200VA 120V"},
+{"0XA27A","Phoenix Inverter 24V 1200VA 120V"},
+{"0XA27C","Phoenix Inverter 48V 1200VA 120V"},
+{"0XA281","Phoenix Inverter 12V 1600VA 230V"},
+{"0XA282","Phoenix Inverter 24V 1600VA 230V"},
+{"0XA284","Phoenix Inverter 48V 1600VA 230V"},
+{"0XA291","Phoenix Inverter 12V 2000VA 230V"},
+{"0XA292","Phoenix Inverter 24V 2000VA 230V"},
+{"0XA294","Phoenix Inverter 48V 2000VA 230V"},
+{"0XA2A1","Phoenix Inverter 12V 3000VA 230V"},
+{"0XA2A2","Phoenix Inverter 24V 3000VA 230V"},
+{"0XA2A4","Phoenix Inverter 48V 3000VA 230V"},
+{"0XA340","Phoenix Smart IP43 Charger 12|50 (1+1)"},
+{"0XA341","Phoenix Smart IP43 Charger 12|50 (3)"},
+{"0XA342","Phoenix Smart IP43 Charger 24|25 (1+1)"},
+{"0XA343","Phoenix Smart IP43 Charger 24|25 (3)"},
+{"0XA344","Phoenix Smart IP43 Charger 12|30 (1+1)"},
+{"0XA345","Phoenix Smart IP43 Charger 12|30 (3)"},
+{"0XA346","Phoenix Smart IP43 Charger 24|16 (1+1)"},
+{"0XA347","Phoenix Smart IP43 Charger 24|16 (3)"},
+{"0XA381","BMV-712 Smart"},
+{"0XA382","BMV-710H Smart"},
+{"0XA383","BMV-712 Smart Rev2"},
+{"0XA389","SmartShunt 500A/50mV"},
+{"0XA38A","SmartShunt 1000A/50mV"},
+{"0XA38B","SmartShunt 2000A/50mV"},
+{"0XA3F0","Smart BuckBoost 12V/12V-50A"},
 
 //data for the Ve Simulator
 {"0X9999", "Ve Sim"}
+
 };
-/*
- 
-SmartSolar MPPT 150|70 rev2 0XA06C
-SmartSolar MPPT 150|85 rev3 0XA06D
-SmartSolar MPPT 150|100 rev3 0XA06E
-BlueSolar MPPT 150|45 rev2 0XA06F
-BlueSolar MPPT 150|60 rev2 0XA070
-BlueSolar MPPT 150|70 rev2 0XA071
-BlueSolar MPPT 150/45 rev3 0XA072
-SmartSolar MPPT 150/45 rev3 0XA073
-SmartSolar MPPT 75/10 rev2 0XA074
-SmartSolar MPPT 75/15 rev2 0XA075
-BlueSolar MPPT 100/30 rev3 0XA076
-BlueSolar MPPT 100/50 rev3 0XA077
-BlueSolar MPPT 150/35 rev3 0XA078
-BlueSolar MPPT 75/10 rev2 0XA079
-BlueSolar MPPT 75/15 rev2 0XA07A
-BlueSolar MPPT 100/15 rev2 0XA07B
-BlueSolar MPPT 75/10 rev3 0XA07C
-BlueSolar MPPT 75/15 rev3 0XA07D
-SmartSolar MPPT 100/30 12V 0XA07E
-All-In-1 SmartSolar MPPT 75/15 12V 0XA07F
-SmartSolar MPPT VE.Can 150/70 0XA102
-SmartSolar MPPT VE.Can 150/45 0XA103
-SmartSolar MPPT VE.Can 150/60 0XA104
-SmartSolar MPPT VE.Can 150/85 0XA105
-SmartSolar MPPT VE.Can 150/100 0XA106
-SmartSolar MPPT VE.Can 250/45 0XA107
-SmartSolar MPPT VE.Can 250/60 0XA108
-SmartSolar MPPT VE.Can 250/70 0XA109
-SmartSolar MPPT VE.Can 250/85 0XA10A
-SmartSolar MPPT VE.Can 250/100 0XA10B
-SmartSolar MPPT VE.Can 150/70 rev2 0XA10C
-SmartSolar MPPT VE.Can 150/85 rev2 0XA10D
-SmartSolar MPPT VE.Can 150/100 rev2 0XA10E
-BlueSolar MPPT VE.Can 150/100 0XA10F
-BlueSolar MPPT VE.Can 250/70 0XA112
-BlueSolar MPPT VE.Can 250/100 0XA113
-SmartSolar MPPT VE.Can 250/70 rev2 0XA114
-SmartSolar MPPT VE.Can 250/100 rev2 0XA115
-SmartSolar MPPT VE.Can 250/85 rev2 0XA116
-BlueSolar MPPT VE.Can 150/100 rev2 0XA117
-Phoenix Inverter 12V 250VA 230V* 0XA201*
-Phoenix Inverter 24V 250VA 230V* 0XA202*
-Phoenix Inverter 48V 250VA 230V* 0XA204*
-Phoenix Inverter 12V 375VA 230V* 0XA211*
-Phoenix Inverter 24V 375VA 230V* 0XA212*
-Phoenix Inverter 48V 375VA 230V* 0XA214*
-Phoenix Inverter 12V 500VA 230V* 0XA221*
-Phoenix Inverter 24V 500VA 230V* 0XA222*
-Phoenix Inverter 48V 500VA 230V* 0XA224*
-Phoenix Inverter 12V 250VA 230V 0XA231
-Phoenix Inverter 24V 250VA 230V 0XA232
-Phoenix Inverter 48V 250VA 230V 0XA234
-Phoenix Inverter 12V 250VA 120V 0XA239
-Phoenix Inverter 24V 250VA 120V 0XA23A
-Phoenix Inverter 48V 250VA 120V 0XA23C
-Phoenix Inverter 12V 375VA 230V 0XA241
-Phoenix Inverter 24V 375VA 230V 0XA242
-Phoenix Inverter 48V 375VA 230V 0XA244
-Phoenix Inverter 12V 375VA 120V 0XA249
-Phoenix Inverter 24V 375VA 120V 0XA24A
-Phoenix Inverter 48V 375VA 120V 0XA24C
-Phoenix Inverter 12V 500VA 230V 0XA251
-Phoenix Inverter 24V 500VA 230V 0XA252
-Phoenix Inverter 48V 500VA 230V 0XA254
-Phoenix Inverter 12V 500VA 120V 0XA259
-Phoenix Inverter 24V 500VA 120V 0XA25A
-Phoenix Inverter 48V 500VA 120V 0XA25C
-Phoenix Inverter 12V 800VA 230V 0XA261
-Phoenix Inverter 24V 800VA 230V 0XA262
-Phoenix Inverter 48V 800VA 230V 0XA264
-Phoenix Inverter 12V 800VA 120V 0XA269
-Phoenix Inverter 24V 800VA 120V 0XA26A
-Phoenix Inverter 48V 800VA 120V 0XA26C
-Phoenix Inverter 12V 1200VA 230V 0XA271
-Phoenix Inverter 24V 1200VA 230V 0XA272
-Phoenix Inverter 48V 1200VA 230V 0XA274
-Phoenix Inverter 12V 1200VA 120V 0XA279
-Phoenix Inverter 24V 1200VA 120V 0XA27A
-Phoenix Inverter 48V 1200VA 120V 0XA27C
-Phoenix Inverter 12V 1600VA 230V 0XA281
-Phoenix Inverter 24V 1600VA 230V 0XA282
-Phoenix Inverter 48V 1600VA 230V 0XA284
-Phoenix Inverter 12V 2000VA 230V 0XA291
-Phoenix Inverter 24V 2000VA 230V 0XA292
-Phoenix Inverter 48V 2000VA 230V 0XA294
-Phoenix Inverter 12V 3000VA 230V 0XA2A1
-Phoenix Inverter 24V 3000VA 230V 0XA2A2
-Phoenix Inverter 48V 3000VA 230V 0XA2A4
-Phoenix Smart IP43 Charger 12|50 (1+1) 0XA340
-Phoenix Smart IP43 Charger 12|50 (3) 0XA341
-Phoenix Smart IP43 Charger 24|25 (1+1) 0XA342
-Phoenix Smart IP43 Charger 24|25 (3) 0XA343
-Phoenix Smart IP43 Charger 12|30 (1+1) 0XA344
-Phoenix Smart IP43 Charger 12|30 (3) 0XA345
-Phoenix Smart IP43 Charger 24|16 (1+1) 0XA346
-Phoenix Smart IP43 Charger 24|16 (3) 0XA347
-BMV-712 Smart 0XA381
-BMV-710H Smart 0XA382
-BMV-712 Smart Rev2 0XA383
-SmartShunt 500A/50mV 0XA389
-SmartShunt 1000A/50mV 0XA38A
-SmartShunt 2000A/50mV 0XA38B
-Smart BuckBoost 12V/12V-50A 0XA3F0
-*/
